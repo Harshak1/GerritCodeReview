a47bb2aaa6e93e72a37a7ff167a5ef2899e8faa7
knsubbu <knsubbu@hotmail.com>
2016-05-18 22:11:18 +0530

    Second commit.

--- src/com/subbu/HelloWorld.java
+++ src/com/subbu/HelloWorld.java
@@ -33,6 +33,7 @@ public class HelloWorld extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
